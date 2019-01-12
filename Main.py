40de460ee2dc091163a6ec0f7f8e7ec35028af13
abdullahalzoabi <abdullahalzoabi.zo@gmail.com>
2019-01-12 17:02:14 +0200

    Download datasets and read first one

---
+++ Main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+
+
+def ReadKinase():
+
+    DDSimilarity = pd.read_csv('Datasets\\Kinase_DD.txt', sep=" " , header = None);
+
+    DDSimilarity = DDSimilarity.drop([0], axis=1)
+
+    TTSimilarity = pd.read_csv('Datasets\\Kinase_TT.txt', sep=" " , header = None);
+
+    TTSimilarity = TTSimilarity.drop([0], axis=1)
+
+    Interactions = pd.read_csv('Datasets\\Kinase_Interactions.txt', sep=" " , header = None);
+
+    Interactions = Interactions.drop([0], axis=1)
+
+    return {"DDSimilarity":DDSimilarity, "TTSimilarity":TTSimilarity, "Interactions":Interactions};
+
+
+
+
+
+Kinase = ReadKinase();
+
