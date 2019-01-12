576850fc6d5c04a88f814ebac4f42ed1af1bd0de
abdullahalzoabi <abdullahalzoabi.zo@gmail.com>
2019-01-13 01:21:16 +0200

    Add"DrugBasedPrediction" fuction

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 
@@ -7,21 +8,78 @@ def ReadKinase():
 
     DDSimilarity = pd.read_csv('Datasets\\Kinase_DD.txt', sep=" " , header = None);
 
-    DDSimilarity = DDSimilarity.drop([0], axis=1)
+    DDSimilarity = DDSimilarity.drop([0], axis=1);
 
     TTSimilarity = pd.read_csv('Datasets\\Kinase_TT.txt', sep=" " , header = None);
 
-    TTSimilarity = TTSimilarity.drop([0], axis=1)
+    TTSimilarity = TTSimilarity.drop([0], axis=1);
 
     Interactions = pd.read_csv('Datasets\\Kinase_Interactions.txt', sep=" " , header = None);
 
-    Interactions = Interactions.drop([0], axis=1)
+    Interactions = Interactions.drop([0], axis=1);
 
     return {"DDSimilarity":DDSimilarity, "TTSimilarity":TTSimilarity, "Interactions":Interactions};
 
 
 
+Data = ReadKinase();
 
 
-Kinase = ReadKinase();
+DDSimilarity = Data["DDSimilarity"];
+
+
+TTSimilarity = Data["TTSimilarity"];
+
+
+Interactions = Data["Interactions"];
+
+
+NumOfDrugs = Interactions.shape[0];
+
+
+NumOfTargets = Interactions.shape[1];
+
+
+NumOfNeighbours = 68; 
+
+
+def DrugBasedPrediction(i,j):
+
+    Numerator  = 0;
+
+    Denominator = 0;
+
+    SortedIndices = np.argsort(-DDSimilarity.iloc[i,:]);
+
+    Count = 0;
+
+    for k in range(0, NumOfDrugs):
+
+        CurrentIndex = SortedIndices.iloc[k,];
+
+        if CurrentIndex != i:
+
+            Numerator = Numerator + Interactions.iloc[CurrentIndex,j]*DDSimilarity.iloc[i,CurrentIndex];
+
+            Denominator = Denominator + DDSimilarity.iloc[i,CurrentIndex];
+
+            Count = Count + 1;
+
+            if Count == NumOfNeighbours:
+
+                break;
+
+    return Numerator/Denominator;
+
+
+print(DrugBasedPrediction(60,54));
+    
+
+
+    
+
+    
+
+
+
 
